a6ade1fd88e7d5ec00445b0d471a1e5defea3cd0
susumoa <tobiasbori@gmail.com>
2020-04-29 13:18:35 +0100

    fet: install webpach and jest, add webpack config files

---
+++ webpack.dev.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
+  entry: './src/client/index.js',
+  module: {},
+  plugins: [],
+};
---
+++ webpack.prod.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const webpack = require('webpack');
+module.exports = {
+  mode: 'production',
+  devtool: 'source-map',
+  entry: './src/client/index.js',
+  module: {},
+  plugins: [],
+};
